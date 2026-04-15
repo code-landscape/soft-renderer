e2e75d226d948b4aafb30cf7ca21062fee89da3f
P1xed <990512962@qq.com>
2026-04-15 20:50:28 +0800

    test drawing something on SDL3

--- tests/imgui_viewer/main.cpp
+++ tests/imgui_viewer/main.cpp
@@ -17,6 +17,8 @@
 #include "backends/imgui_impl_sdlrenderer3.h"
 #include "imgui.h"
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_rect.h>
+#include <SDL3/SDL_render.h>
 #include <stdio.h>
 
 #ifdef __EMSCRIPTEN__
@@ -84,29 +86,6 @@ int main(int, char **) {
   ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
   ImGui_ImplSDLRenderer3_Init(renderer);
 
-  // Load Fonts
-  // - If fonts are not explicitly loaded, Dear ImGui will select an embedded
-  // font: either AddFontDefaultVector() or AddFontDefaultBitmap().
-  //   This selection is based on (style.FontSizeBase * style.FontScaleMain *
-  //   style.FontScaleDpi) reaching a small threshold.
-  // - You can load multiple fonts and use ImGui::PushFont()/PopFont() to select
-  // them.
-  // - If a file cannot be loaded, AddFont functions will return a nullptr.
-  // Please handle those errors in your code (e.g. use an assertion, display an
-  // error and quit).
-  // - Read 'docs/FONTS.md' for more instructions and details.
-  // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use FreeType
-  // for higher quality font rendering.
-  // - Remember that in C/C++ if you want to include a backslash \ in a string
-  // literal you need to write a double backslash \\ !
-  // style.FontSizeBase = 20.0f;
-  // io.Fonts->AddFontDefaultVector();
-  // io.Fonts->AddFontDefaultBitmap();
-  // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf");
-  // io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf");
-  // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf");
-  // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf");
-  // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf");
   ImFont *font = io.Fonts->AddFontFromFileTTF(
       "/usr/share/fonts/TTF/JetBrainsMonoNerdFont-Bold.ttf");
   IM_ASSERT(font != nullptr);
@@ -161,55 +140,10 @@ int main(int, char **) {
     ImGui_ImplSDL3_NewFrame();
     ImGui::NewFrame();
 
-    // 1. Show the big demo window (Most of the sample code is in
-    // ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear
-    // ImGui!).
-    if (show_demo_window)
-      ImGui::ShowDemoWindow(&show_demo_window);
-
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair
-    // to create a named window.
     {
-      static float f = 0.0f;
-      static int counter = 0;
-
-      ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!"
-                                     // and append into it.
-
-      ImGui::Text("This is some useful text."); // Display some text (you can
-                                                // use a format strings too)
-      ImGui::Checkbox(
-          "Demo Window",
-          &show_demo_window); // Edit bools storing our window open/close state
-      ImGui::Checkbox("Another Window", &show_another_window);
-
-      ImGui::SliderFloat("float", &f, 0.0f,
-                         1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
-      ImGui::ColorEdit3(
-          "clear color",
-          (float *)&clear_color); // Edit 3 floats representing a color
-
-      if (ImGui::Button("Button")) // Buttons return true when clicked (most
-                                   // widgets return true when edited/activated)
-        counter++;
-      ImGui::SameLine();
-      ImGui::Text("counter = %d", counter);
-
-      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-                  1000.0f / io.Framerate, io.Framerate);
-      ImGui::End();
-    }
-
-    // 3. Show another simple window.
-    if (show_another_window) {
-      ImGui::Begin(
-          "Another Window",
-          &show_another_window); // Pass a pointer to our bool variable (the
-                                 // window will have a closing button that will
-                                 // clear the bool when clicked)
-      ImGui::Text("Hello from another window!");
-      if (ImGui::Button("Close Me"))
-        show_another_window = false;
+      ImGui::Begin("test");
+      ImGui::Text("ImText");
+      ImGui::ColorEdit3("clear_color", &clear_color.x);
       ImGui::End();
     }
 
@@ -220,6 +154,13 @@ int main(int, char **) {
     SDL_SetRenderDrawColorFloat(renderer, clear_color.x, clear_color.y,
                                 clear_color.z, clear_color.w);
     SDL_RenderClear(renderer);
+
+    // draw my own stuff here
+    SDL_SetRenderDrawColorFloat(renderer, 0, 0, 0, 255);
+    const SDL_FRect rect = {100, 100, 200, 150};
+    SDL_RenderFillRect(renderer, &rect);
+    SDL_SetRenderDrawColorFloat(renderer, clear_color.x, clear_color.y,
+                                clear_color.z, clear_color.w);
     ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
     SDL_RenderPresent(renderer);
   }
